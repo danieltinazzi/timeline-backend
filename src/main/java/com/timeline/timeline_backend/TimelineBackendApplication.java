f8abc6bca127aec21b5f58a7f74c0075981fe7f9
Daniel Tinazzi <daniel.tinazzi@outlook.com>
2025-09-05 23:45:37 +0200

    Initial commit

---
+++ src/main/java/com/timeline/timeline_backend/TimelineBackendApplication.java
@@ -0,0 +1,13 @@
+package com.timeline.timeline_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimelineBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimelineBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timeline/timeline_backend/TimelineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timeline.timeline_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimelineBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
