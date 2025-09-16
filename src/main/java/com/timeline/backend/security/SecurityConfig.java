ffd150f8bfdfd2b35a1c628ee25bdbd993ef74f4
Daniel Tinazzi <daniel.tinazzi@outlook.com>
2025-09-16 23:48:33 +0200

    Add api version

--- src/main/java/com/timeline/backend/security/SecurityConfig.java
+++ src/main/java/com/timeline/backend/security/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
             .csrf(AbstractHttpConfigurer::disable)
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
-                    .requestMatchers("/api/auth/**").permitAll()
+                    .requestMatchers("/api/v1/auth/**").permitAll()
                     .anyRequest().authenticated()
             )
             .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
