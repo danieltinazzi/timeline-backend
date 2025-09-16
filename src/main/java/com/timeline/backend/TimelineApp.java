0e1f7044aa1ce8d8ef965819242a4f11270be9de
Daniel Tinazzi <daniel.tinazzi@outlook.com>
2025-09-16 23:47:59 +0200

    Rename classes

--- src/main/java/com/timeline/backend/TimelineApp.java
+++ src/main/java/com/timeline/backend/TimelineApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TimelineBackendApplication {
+public class TimelineApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TimelineBackendApplication.class, args);
+		SpringApplication.run(TimelineApp.class, args);
 	}
 
 }
